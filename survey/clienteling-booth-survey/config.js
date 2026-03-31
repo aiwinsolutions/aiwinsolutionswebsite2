a663c9955c4eec66ec1ad97b093129faa9e8cc88
aiwinsolutions <aiwinsolutions@users.noreply.github.com>
2026-03-31 06:17:35 +0000

    deploy: ffa678276f3f9000a8f929491af2205692eb3370

--- survey/clienteling-booth-survey/config.js
+++
@@ -1,5 +0,0 @@
-// ============================================================
-// Supabase credentials
-// ============================================================
-const SUPABASE_URL      = 'https://evcjctuvvysqhmsdrwom.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Y2pjdHV2dnlzcWhtc2Ryd29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNzU0MzUsImV4cCI6MjA4ODg1MTQzNX0.UIUBGn-D7T7qPlDiv3LN5_w29sEYl9ObH6tVJlIj-H0';
